bf885e0480371480f82612a1ef1a852238734353
Daniel Scislowski <mr.scislowski@gmail.com>
2023-04-18 16:48:58 -0500

    implement filtering through checkboxes

--- TODO.md
+++ TODO.md
@@ -1,3 +1,7 @@
-[ ] checking checkbox causes filter to change
+[ ] clean up gross quotes in the data
+[ ] implement question context
+[ ]
+[ ]
+[ ]
 [ ] get different filtering options, like, at the moment it's doing OR; we could make it do AND instead. Also, we could exclude things that were later in the curriculum, etc
 [ ] make it look nicer with, like, flexbox
--- src/App.js
+++ src/App.js
@@ -6,38 +6,21 @@ import { QuestionSet } from './QuestionSet';
 const questionList = require("./assets/Categorization.json")
 const topicList = require("./assets/APTopicNames.json")
 
-const allTopics = new Set();
-questionList.forEach(question => {
-  question.topics.forEach(topic => {
-    allTopics.add(topic);
-  })
-})
-
-
-const filterQuestions = (allQuestions, selectedTopics, additionalTopics) => {
-  const result = [];
-  for (let question of allQuestions) {
-    if (additionalTopics.map(topic => question.topics.includes(topic)).includes(false)) {
-      // do nothing
-    } else {
-      for (let topic of question.topics) {
-        if (selectedTopics.includes(topic)) {
-          result.push(question);
-          break;
-        }
+const filterQuestions = (allQuestions, selectedTopicIds) => {
+  const result = allQuestions.filter(question => {
+    for (let topic of selectedTopicIds) {
+      if (question.topics.includes(topic)) {
+        return true;
       }
     }
-  }
+    return false;
+  });
+
   return result;
 }
 
-
-
-
 function App() {
   const [checkboxesState, setCheckboxesState] = useState(new Array(topicList.length).fill(false));
-  const [selectedTopics, setSelectedTopics] = useState([]);
-  const [additionalTopics, setAdditionalTopics] = useState([]);
 
   const handleCheckboxChange = (position) => {
     const updatedCheckedState = checkboxesState.map((item, index) => 
@@ -45,7 +28,9 @@ function App() {
     setCheckboxesState(updatedCheckedState);
   }
 
-  const filteredQuestions = filterQuestions(questionList, selectedTopics, additionalTopics);
+  const selectedTopics = topicList.filter((element, index) => checkboxesState[index] === true);
+  const filteredQuestions = filterQuestions(questionList, 
+    selectedTopics.map(el => el.ApTopicId));
 
   return (
     <>
