cc8b6224e3b3678a0435dc379a59517382f5f9fe
Daniel Scislowski <mr.scislowski@gmail.com>
2023-04-19 21:23:47 -0500

    implement context

--- TODO.md
+++ TODO.md
@@ -1,7 +1,4 @@
-[ ] implement question context
 [ ]
 [ ]
 [ ]
-[ ] get different filtering options, like, at the moment it's doing OR; we could make it do AND instead. Also, we could exclude things that were later in the curriculum, etc
 [ ] make it look nicer with, like, flexbox
-Newton’s First Law
--- src/App.js
+++ src/App.js
@@ -6,21 +6,49 @@ import { QuestionSet } from './QuestionSet';
 const questionList = require("./assets/Categorization.json")
 const topicList = require("./assets/APTopicNames.json")
 
-const filterQuestions = (allQuestions, selectedTopicIds) => {
-  const result = allQuestions.filter(question => {
-    for (let topic of selectedTopicIds) {
-      if (question.topics.includes(topic)) {
-        return true;
+const filterQuestions = (allQuestions, selectedTopicIds, withContext) => {
+  let result = [];
+  if (!withContext) {
+    result = allQuestions.filter(question => {
+      for (let topic of selectedTopicIds) {
+        if (question.topics.includes(topic)) {
+          return true;
+        }
       }
-    }
-    return false;
-  });
+      return false;
+    });
+  } else {
+    // TODO: maybe use array group method
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/group
+    const grouped = allQuestions.reduce((group, currentQuestion) => {
+      const category = `${currentQuestion.year}Q${currentQuestion.question}`;
+      if (!group.hasOwnProperty(category)) {
+        group[category] = [];
+      }
+    
+      currentQuestion.topics.forEach(topic => {
+        group[category].push(topic);
+      });
+    
+      return group;
+    }, {});
+
+    result = allQuestions.filter(question => {
+      for (let topic of selectedTopicIds) {
+        if (grouped[`${question.year}Q${question.question}`].includes(topic)) {
+          return true;
+        }
+      }
+      return false;
+    });
+  }
 
   return result;
 }
 
 function App() {
   const [checkboxesState, setCheckboxesState] = useState(new Array(topicList.length).fill(false));
+  const [includeContext, setIncludeContext] = useState(false);
 
   const handleCheckboxChange = (position) => {
     const updatedCheckedState = checkboxesState.map((item, index) => 
@@ -30,10 +58,19 @@ function App() {
 
   const selectedTopics = topicList.filter((element, index) => checkboxesState[index] === true);
   const filteredQuestions = filterQuestions(questionList, 
-    selectedTopics.map(el => el.ApTopicId));
-
+    selectedTopics.map(el => el.ApTopicId), includeContext);
+  
   return (
     <>
+    <input 
+      type="checkbox"
+      id="include-context-checkbox"
+      name="include question context"
+      value="include question context"
+      checked={includeContext}
+      onChange={() => setIncludeContext(!includeContext)} />
+    <label htmlFor="include-context-checkbox">Include question context?</label>
+
       {topicList.map((topicInfo, index) => {
         return (
           <li key={`custom-checkbox-${index}`}>
